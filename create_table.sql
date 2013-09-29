519bb440eee01b074d2ee41ed3ad446bbde595dc
katryo <katoryo55@gmail.com>
2013-09-29 20:04:21 +0900

    vagrantで動くようにした

--- create_table.sql
+++ create_table.sql
@@ -3,6 +3,8 @@ USE kossy_training;
 CREATE TABLE todos (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name TEXT NOT NULL,
-    created_at TIMESTAMP default current_timestamp,
-    updated_at TIMESTAMP
+    created_at datetime NOT NULL,
+    updated_at TIMESTAMP on update current_timestamp,
+    deadline datetime,
+    comment text
 )
--- lib/KossyTraining/Web.pm
+++ lib/KossyTraining/Web.pm
@@ -3,6 +3,7 @@ package KossyTraining::Web;
 use strict;
 use warnings;
 use utf8;
+use DateTime;
 use Kossy;
 use KossyTraining::Model::DB;
 
@@ -37,10 +38,36 @@ post '/' => sub {
     ]);
     my $name = $result->valid->get('name');
     my $row = $teng->insert('todos' => {
-        name => $name
+        name => $name,
+        created_at => DateTime->now(time_zone => 'Asia/Tokyo')
     });
     my $iter = $teng->search('todos');
     $c->render('index.tx', { info => "we got " . $name , iter => $iter });
+};
+
+get '/{id:[0-9]+}/edit' => sub{
+    my ( $self, $c ) = @_;
+    my $row = $teng->single('todos', {"id" => $c->args->{id}});
+    if ($row){
+        $c->render('edit.tx', {row => $row});
+    }
+    else{
+        $c->halt(404, "file not found");
+    }
+};
+
+post '/{id:[0-9]+}/update' => sub{
+    my ( $self, $c ) = @_;
+    my $id = $c->args->{id};
+    my $result = $c->req->validator([
+        'name' => {
+            rule => [
+                ['NOT_NULL','empty body'],
+            ],
+        }]);
+    my $name = $result->valid->get('name');
+    my $record = $teng->update('todos', +{name => $name}, +{id => $id});
+    $c->redirect($c->req->uri_for('/'));
 };
 
 post '/{id:[0-9]+}/delete' => sub{
